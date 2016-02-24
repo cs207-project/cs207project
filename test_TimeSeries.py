3729a047209e89e7df7d00b00696409583b9650f
CHENCHEN ZHANG <MikeZhang@dhcp-140-247-103-68.fas.harvard.edu>
2016-02-23 20:15:08 -0500

    Added test cases for len and contains

--- test_TimeSeries.py
+++ test_TimeSeries.py
@@ -2,6 +2,16 @@ from pytest import raises
 import TimeSeries as TS
 from lazy import *
 
+testSeries = TS.TimeSeries(range(0,4),range(1,5))
+
+def test_len();
+    assert len(testSeries) == 5
+
+def test_contains():
+    assert (2 in testSeries) == True
+
+def test_len(
+
 @lazy
 def check_length(a,b):
     return len(a)==len(b)
