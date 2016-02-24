6e49c45ce486524a59c6520e4d3b601522409c9e
CHENCHEN ZHANG <MikeZhang@dhcp-140-247-103-68.fas.harvard.edu>
2016-02-23 19:44:00 -0500

    Added TS in file

--- test_TimeSeries.py
+++ test_TimeSeries.py
@@ -5,5 +5,5 @@ from lazy import *
 @lazy
 def check_length(a,b):
     return len(a)==len(b)
-thunk = check_length(TimeSeries(range(0,4),range(1,5)).lazy, TimeSeries(range(1,5),range(2,6)))
+thunk = check_length(TS.TimeSeries(range(0,4),range(1,5)).lazy, TS.TimeSeries(range(1,5),range(2,6)))
 assert thunk.eval()==True
