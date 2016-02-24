f1dcc4f7280c4ac3e55585764c91e0439e48420e
CHENCHEN ZHANG <MikeZhang@dhcp-140-247-103-68.fas.harvard.edu>
2016-02-23 19:49:03 -0500

    Added prefix to test method

--- test_TimeSeries.py
+++ test_TimeSeries.py
@@ -5,5 +5,7 @@ from lazy import *
 @lazy
 def check_length(a,b):
     return len(a)==len(b)
-thunk = check_length(TS.TimeSeries(range(0,4),range(1,5)).lazy, TS.TimeSeries(range(1,5),range(2,6)))
-assert thunk.eval()==True
+
+def test_check_length():
+    thunk = check_length(TS.TimeSeries(range(0,4),range(1,5)).lazy, TS.TimeSeries(range(1,5),range(2,6)))
+    assert thunk.eval()==True
