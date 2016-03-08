41ce41ea7d90cfa33c887ae5f8457c8c6a9a0834
ytang14 <tang01@g.harvard.edu>
2016-03-08 15:19:23 -0500

    added pos & neg

--- test_TimeSeries.py
+++ test_TimeSeries.py
@@ -81,7 +81,7 @@ def test_add():
     assert c+a == a+c
     with raises(ValueError):
         d+a
-        
+
 def test_sub():
     a = TS.TimeSeries([0,5,10], [1,2,3])
     b = TS.TimeSeries([0,5,10], [10,20,30])
@@ -91,6 +91,15 @@ def test_sub():
     assert a-c == TS.TimeSeries([0,5,10],[-99,-98,-97])
     with raises(ValueError):
         a-d
+
+def test_neg():
+    a = TS.TimeSeries([0,5,10], [1,2,3])
+    b = TS.TimeSeries([0,5,10], [10,20,30])
+    assert -a == TS.TimeSeries([0,5,10],[-1,-2,-3])
+
+def test_pos():
+    a = TS.TimeSeries([0,5,10], [-1,-2,3])
+    assert +a == a
 @lazy
 def check_length(a,b):
     return len(a)==len(b)
